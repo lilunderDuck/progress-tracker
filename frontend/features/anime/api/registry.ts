d1efb3fb5e88d25db4ef4b501083ddf26433a159
mint_duck <thisisjustalongtext@gmail.com>
2026-08-18 21:07:17 +0700

    changing some registry

--- frontend/features/anime/api/registry.ts
+++ frontend/features/anime/api/registry.ts
@@ -9,7 +9,7 @@ export const SCORE_NOTES_REGISTRY: Record<AnimeTrackerNote, {
   [AnimeTrackerNote.USED_SCORE_AVG_ANIME]: {
     name$: "Anime score used averaging",
     note$: null,
-    description$: "Score is calculated by taking the average of all seasons"
+    description$: "Score is calculated by taking the average of all anime seasons"
   },
   [AnimeTrackerNote.USED_SCORE_AVG_MANGA]: {
     name$: "Manga score used averaging",
@@ -17,12 +17,12 @@ export const SCORE_NOTES_REGISTRY: Record<AnimeTrackerNote, {
     note$: null,
   },
   [AnimeTrackerNote.INCLUDED_SPECIAL_EPS_SCORE]: {
-    name$: "Score included special episodes",
+    name$: "Score included special episodes score",
     description$: "Calulated score *does* include the scores from special episodes (OVA, ...)",
     note$: null,
   },
   [AnimeTrackerNote.NOT_INCLUDED_SPECIAL_EPS_SCORE]: {
-    name$: "Score did not include special episodes",
+    name$: "Score did not include special episodes score",
     description$: "Calulated score *does not* include the scores from special episodes (OVA, ...)",
     note$: "Use this if you have entries with the same name but with 2 different progress status."
   },
@@ -33,7 +33,7 @@ export const SCORE_NOTES_REGISTRY: Record<AnimeTrackerNote, {
   },
   [AnimeTrackerNote.MANGADEX_SCORE_ONLY]: {
     name$: "Score is taken from mangadex",
-    description$: "...since there's no rating on MAL, and Mangadex ratings tend to be inflated.",
+    description$: "...since there's no rating on MAL. Note that Mangadex ratings tend to be inflated.",
     note$: "Use this if MAL listed the score as \"not available\""
   },
   [AnimeTrackerNote.USED_MAL_INSTEAD_OF_MANGADEX]: {
@@ -43,7 +43,7 @@ export const SCORE_NOTES_REGISTRY: Record<AnimeTrackerNote, {
   },
   [AnimeTrackerNote.NOT_LIGHT_NOVEL]: {
     name$: "Score is from a novel, not a light novel.",
-    description$: "Reality... this is from a novel, but it have been grouped under '(L)N' for simplicity.",
+    description$: "This is from a novel, but it have been grouped under '(L)N' for simplicity. (novel is mostly text only, light novel includes anime-style illustrations)",
     note$: "To simplify, the table grouped both of the \"novel\" and \"light novel\" into one column to make it cleaner. If an entry only exists the \"novel\" version, add this to clarify.",
   },
   [AnimeTrackerNote.SCORE_NOT_AVAILABLE]: {
@@ -58,11 +58,11 @@ export const SCORE_NOTES_REGISTRY: Record<AnimeTrackerNote, {
   },
   [AnimeTrackerNote.FROM_YT]: {
     name$: "This can be found on youtube",
-    description$: "Note that there's a small chance that some episode(s) are not available, because youtube can be youtube-y sometime",
+    description$: "You can find it by searching the name. Note that there's a small chance that some episode(s) are not available, youtube can be unpredictable sometimes",
     note$: "This can be found on youtube (via MuseAsia or some random channel) by searching the name",
   },
   [AnimeTrackerNote.LOCAL_ARCHIVE]: {
-    name$: "A local copy of this has been saved",
+    name$: "Archived",
     description$: "\"I've made a full 100% local copy of this\"",
     note$: null,
   },
@@ -73,12 +73,12 @@ export const SCORE_NOTES_REGISTRY: Record<AnimeTrackerNote, {
   },
   [AnimeTrackerNote.MANGADEX_MISSING_CHAPTERS]: {
     name$: "Missing chapters in mangadex",
-    description$: "Some of the chapters is not translated into english, or all chapters are being listed as \"This chapter is not available\"",
+    description$: "Some of the chapters are not translated into english, or all chapters are being listed as \"This chapter is not available\"",
     note$: null
   },
   [AnimeTrackerNote.ONE_SHOT]: {
     name$: "This is a one-shot",
-    description$: "(.statement) - a one chapter story only, and usually a short story.",
+    description$: "(.statement) - basically a one chapter story only, and usually a short story.",
     note$: null
   }
 }
